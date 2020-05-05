68fac4c83e3b0e2796f26dcf2dc82bf0e8ba8c93
afmdanielwin <danielfarias380@gmail.com>
2020-05-05 20:23:15 -0300

    implement footer component

--- src/components/template/Footer.jsx
+++ src/components/template/Footer.jsx
@@ -4,9 +4,11 @@ import React from 'react';
 function Footer(props) {
     return (
         <footer className="footer">
-            Rodapé
+            <span>
+                Developed with <i className="fa fa-heart text-danger"></i> by <strong>Daniel Melo</strong>
+            </span>
         </footer>
-    )
+    );
 }
 
 export default Footer;
