0a693d54c26f38c4f15bf6fcc89204ca09e932a9
afmdanielwin <danielfarias380@gmail.com>
2020-05-05 20:41:59 -0300

    implement Nav component

--- src/components/template/Nav.jsx
+++ src/components/template/Nav.jsx
@@ -4,7 +4,14 @@ import React from 'react';
 function Nav(props) {
     return (
         <aside className="menu-area">
-            Navegação
+            <nav className="menu">
+                <a href="#/">
+                    <i className="fa fa-home"></i> Home
+                </a>
+                <a href="#/users">
+                    <i className="fa fa-users"></i> Users
+                </a>
+            </nav>
         </aside>
     );
 }
