cfa9e69ce72189b75c3492144d841f228e044fef
afmdanielwin <danielfarias380@gmail.com>
2020-05-04 22:42:00 -0300

    import template components to App file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import Logo from './components/template/Logo';
+import Nav from './components/template/Nav';
+import Main from './components/template/Main';
+import Footer from './components/template/Footer';
+
+function App(props) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Logo />
+      <Nav />
+      <Main />
+      <Footer />
     </div>
   );
 }
