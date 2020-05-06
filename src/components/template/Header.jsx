3d7fe75c290233fb8db9e943d87f168a5db08fe0
afmdanielwin <danielfarias380@gmail.com>
2020-05-06 19:06:59 -0300

    add responsivity

--- src/components/template/Header.jsx
+++ src/components/template/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 function Header(props) {
     return (
-        <header className="header d-none d-sm-flex flex-column">
+        <header className="header d-sm-flex flex-column">
             <h1 className="mt-3">
                 <i className={`fa fa-${props.icon}`}></i> {props.title}
             </h1>
