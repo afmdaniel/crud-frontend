e0f0b8452264676423023006e49f29faa7fae279
afmdanielwin <danielfarias380@gmail.com>
2020-05-05 22:18:13 -0300

    import Link component for navigation

--- src/components/template/Logo.jsx
+++ src/components/template/Logo.jsx
@@ -1,13 +1,14 @@
 import './Logo.css';
 import React from 'react';
 import logo from '../../assets/img/logo.png'
+import { Link } from 'react-router-dom'
 
 function Logo(props) {
     return (
         <aside className="logo">
-            <a href="/" className="logo">
+            <Link to="/" className="logo">
                 <img src={logo} alt="logo" />
-            </a>
+            </Link>
         </aside>
     );
 }
