25acb6c63d60ab80dd59519d0886407fe8fd689b
afmdanielwin <danielfarias380@gmail.com>
2020-05-06 00:24:17 -0300

    implement put request

--- src/components/user/UserCrud.jsx
+++ src/components/user/UserCrud.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import Main from '../template/Main';
+import api from '../../services/api';
+import { useState } from 'react';
+
+import UserForm from './UserForm';
 
 const headerProps = {
     icon: 'users',
@@ -7,10 +11,52 @@ const headerProps = {
     subtitle: 'User Registration: Create, Remove, Update and Delete!'
 }
 
+const initialUser = {
+    name: '',
+    email: ''
+}
+
+const initialList = [];
+
 function UserCrud(props) {
+    const [user, setUser] = useState(initialUser);
+    const [list, setList] = useState(initialList);
+
+    function clear() {
+        setUser(initialUser);
+    }
+    
+    async function save() {
+        const method = user.id ? 'put' : 'post';
+        const endPoint = user.id ? `/${user.id}` : '';
+        
+        const resp = await api[method](endPoint, user);
+        const auxList = getUpdatedList(resp.data);
+        setUser(initialUser);
+        setList(auxList);
+    }
+
+    function getUpdatedList(user) {
+        const auxList = list.filter(u => u.id === user.id);
+        auxList.unshift(user);
+        return auxList;
+    }
+
+    function updateField(event) {
+        const newUser = { ...user }
+        newUser[event.target.name] = event.target.value;
+        setUser({ ...newUser });
+    }
+
     return (
         <Main {...headerProps}>
-            User Registration
+            <UserForm 
+                name={user.name}
+                email={user.email} 
+                handleChange={updateField}
+                onSubmit={save}
+                onCancel={clear}
+            />
         </Main>
     );
 }
