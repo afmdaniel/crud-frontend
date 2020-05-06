b84200dccb829618e4e2b4cf5f8be6af367f1891
afmdanielwin <danielfarias380@gmail.com>
2020-05-05 21:18:24 -0300

    implement Main component

--- src/components/template/Main.jsx
+++ src/components/template/Main.jsx
@@ -6,8 +6,10 @@ function Main(props) {
     return (
         <React.Fragment>
             <Header {...props}/>
-            <main className="content">
-                Conteúdo
+            <main className="content container-fluid">
+                <div className="p-3 mt-3">
+                    {props.children}
+                </div>
             </main>
         </React.Fragment>
     );
